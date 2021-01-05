311426feef42c8e8b89cac2a8a528bc742a00e13
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-01-06 03:04:44 +0900

    로드 개편1

--- KOverload_Mining_Game/mining.js
+++ KOverload_Mining_Game/mining.js
@@ -1,18 +1,26 @@
-var SD = {
+var SD = {}; //현제 세이브파일이 될 예정
+
+var default_SD = { //기본값 세이브파일
     iron : 0,
     gold : 0,
     diamond : 0,
     emerald : 0,
     ruby : 0,
     overlord : 0,
-    upgrade1 : 50,
-    purchased1 : 0,
-    upgrade2 : 2000,
-    purchased2 : 0,
     upgrade3 : 5,
     purchased3 : 0,
+    upgrade4 : 50,
+    purchased4 : 0,
+    upgrade5 : 400,
+    purchased5 : 0,
+    upgrade6 : 200,
+    purchased6 : 0,
     a : 1,
     b : 1,
+    c : 0,
+    d : 0,
+    e : 1,
+    f : 1,
     irongold : 0,
     extractpipe : 0,
     engine : 0,
@@ -28,20 +36,12 @@ var SD = {
     exupgrade1 : 20,
     exupgrade2 : 5,
     expurchased : 0,
-    upgrade4 : 50,
-    purchased4 : 0,
     doubleminingposs : 0,
     enchantedoverlordingot : 0,
     autominetime : 10,
     robotmaker : 0,
     minepower : 0,
     circuit : 0,
-    upgrade5 : 400,
-    purchased5 : 0,
-    upgrade6 : 200,
-    purchased6 : 0,
-    c : 0,
-    d : 0,
     maxrobot : 5,
     robotbody : 0,
     CPU : 0,
@@ -62,22 +62,30 @@ var SD = {
     mythpurchased2 : 0,
     mythupgrade3 : 10,
     mythpurchased3 : 0,
-    e : 1,
-    f : 1,
+    test1 : 1,
     UGS : [
         {
             name : "한번에 얻는 광물 개수 증가",
-            material : "SD.iron"
-
+            material : "SD.iron",          //new Function('return ' + SD.UGS[0].material)()
+            level : 0,
+            price : 50,
+            price_equation : "parseInt((50 + SD.UGS[num].level * (SD.UGS[num].level + 1) * 25) * SD.udc)",           //eval() 이용해서 결제할것
+            result : [
+                ["SD.a", "(parseInt((n + 1)/5) * -5 + 2 * n + 2) * (parseInt((n + 1)/5) + 1)/2;"],
+            ]
         },
         {
             name : "한번에 조합하는 일반 아이템 개수 증가",
-            material : "SD.gold"
+            material : "SD.diamon",
+            level : 0,
+            price : 2000,
+            price_equation : "parseInt((2000 + SD.UGS[num].level * (SD.UGS[num].level + 1) * 1000) * SD.udc)",
+            result : [
+                ["SD.b", "SD.UGS[num].level + 1"]
+            ]
         },
-
     ],
 }
-
 
 function save() { // 세이브
     localStorage['saveFile'] = JSON.stringify(SD);
@@ -85,7 +93,23 @@ function save() { // 세이브
 }
 
 function load() { // 로드
-    SD = JSON.parse(localStorage['saveFile']);
+    var SD_old; //SD_old 세이브파일
+    if(localStorage.hasOwnProperty("saveFile")){ //SD_old 세이브 파일이 있을경우
+        SD_old = JSON.parse(localStorage['saveFile']); //불러온다
+    }
+    
+    for(key in default_SD){ //default_SD 와 SD_old 대조해서
+        if(SD_old.hasOwnProperty(key)){ //키값이 같은게 SD_old에 있을경우 (만약 SD_old에만 있는 키값이 있으면 가져오지 않음)
+            SD[key] = SD_old[key];  //SD_old를 SD 세이브파일로 불러온다
+        }
+    }
+    
+    for(key in default_SD){ //default_SD 와 SD 대조해서
+        if(!SD.hasOwnProperty(key)){    //default_SD 세이브파일에만 존제하는 키를 (SD 세이브파일에 없는거)
+            SD[key] = default_SD[key]; //가져온다
+        }
+    }
+
     add_log("*로드되었습니다*");
     store();
 }
@@ -107,9 +131,9 @@ function add_log(log) { // log를 id="log"인 div에 추가함
     n++;
     var x = document.createElement("p");    //<p></p> 만들고 x 라고하자
     x.innerHTML = log;                      //안에는 log 넣음 -> <p>log</p>
-    x.setAttribute("id", "scroll" + n)      //태그 p의 id="scroll" + n 로 설정, 아이디 붙이는 이유는 자동 스크롤을 위함
+    x.setAttribute("id", "scroll_" + n)      //태그 p의 id="scroll" + n 로 설정, 아이디 붙이는 이유는 자동 스크롤을 위함
     document.getElementById("log").append(x);   //id="log"인 div의 마지막에 위에 만든 x를 넣어줌
-    document.getElementById("scroll" + n).scrollIntoView(true);     //최근에 써진 로그로 스크롤해줌
+    document.getElementById("scroll_" + n).scrollIntoView(true);     //최근에 써진 로그로 스크롤해줌
 }
 
 function store() {  //id="storehouse" 인 div의 내용을 아래에 있는 Store로 설정함
@@ -157,13 +181,16 @@ function mining(num) { // 아시다시피 광질하는거 대충 복사했음
     store(); // 광질 한 후 보유량 업데이트
 }
 
+function UGS_load(list_num) {
+    var y = document.getElementsByClassName("UGS_list");
+    y[list_num].innerHTML = SD["UGS"][list_num]["name"];
+}
+
+function upgrade(num) {
+
+}
+
 
 window.onload = function () { // 페이지가 다 load 되면 store(), load()함수 실행
-    store(); //세이브파일이 없을때 실행해도 보유갯수가 0개로 보이게 하기위해 사용
-    try {    
-        load();
-    }
-    catch {
-        // 비워둘것
-    }
+    load();
 }
