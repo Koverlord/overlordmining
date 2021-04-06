e121d386ee9cbd12fc4c6994fd2644b842136bbb
Koverlord <76400288+Koverlord@users.noreply.github.com>
2021-04-06 15:41:37 +0900

    Update tab(2).js

--- KOverload_Mining_Game/js/tab(2).js
+++ KOverload_Mining_Game/js/tab(2).js
@@ -2,9 +2,9 @@ function melt(ore){
     var a=ore;
     var b=a;
     b+='_ingot';
-    if SD['a']>=0{
-        SD['a']-=1;
-        SD['b']+=1;
+    if SD[a]>=0{
+        SD[a]-=1;
+        SD[b]+=1;
         store()
     }
 }
