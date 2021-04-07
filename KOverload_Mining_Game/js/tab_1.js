1e64f02ec5a60746e504586ef5fd8a96d428fb4a
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-08 00:32:26 +0900

    배낄꺼 주석 추가

--- KOverload_Mining_Game/js/tab_1.js
+++ KOverload_Mining_Game/js/tab_1.js
@@ -53,18 +53,86 @@ function UGS_fail(num) {
     }
 }
 
-// 0 : { //SD['UGS'][num]
-//     name : "한번에 얻는 광물 개수 증가",
-//     material : ["철", "SD['iron']"],
-//     level : 0,
-//     max_level : "-1",
-//     price : 50,
-//     price_equation : "SD[\'UGS\'][num][\'price\'] = parseInt((50 + SD['UGS'][num]['level'] * (SD['UGS'][num]['level'] + 1) * 25) * SD['udc'])",           //eval() 이용해서 결제할것
-//     result : [
-//         ["SD['a']", "(parseInt((SD['UGS'][num]['level'] + 1)/5) * - 5 + 2 * SD['UGS'][num]['level'] + 2) * (parseInt((SD['UGS'][num]['level'] + 1) / 5) + 1) / 2"],
-//     ]
+// UGS : { //SD['UGS']
+//     0 : { //SD['UGS'][num]
+//         name : "한번에 얻는 광물 개수 증가",
+//         material : ["철", "SD['iron']"],
+//         level : 0,
+//         max_level : "-1",
+//         price : 50,
+//         price_equation : "SD[\'UGS\'][num][\'price\'] = parseInt((50 + SD['UGS'][num]['level'] * (SD['UGS'][num]['level'] + 1) * 25) * SD['udc'])",           //eval() 이용해서 결제할것
+//         result : [
+//             ["SD['a']", "(parseInt((SD['UGS'][num]['level'] + 1)/5) * - 5 + 2 * SD['UGS'][num]['level'] + 2) * (parseInt((SD['UGS'][num]['level'] + 1) / 5) + 1) / 2"],
+//         ]
+//     },
+//     1 : {
+//         name : "이중 채광 확률 증가",
+//         material : ["금", "SD['gold']"],
+//         level : 0,
+//         max_level : "100",
+//         price : "50",
+//         price_equation : "SD[\'UGS\'][num][\'price\'] = parseInt((50 + SD['UGS'][num]['level'] * (SD['UGS'][num]['level'] + 1) * 25) * SD['udc'])",
+//         result : [
+//             ["SD.doubleminingposs", ["SD['UGS'][num]['level']"]]
+//         ],
+//     },
+//     2 : {
+//         name : "한번에 얻는 금속 개수 증가",
+//         material : ["에메랄드", "SD['emerald']"],
+//         level : 0,
+//         max_level : -1,
+//         price : "20",
+//         price_equation : "SD[\'UGS\'][num][\'price\'] = parseInt((200 + SD['UGS'][num]['level'] * (SD['UGS'][num]['level'] + 1) * 100) * SD['udc'])",
+//         result : [
+//             ["SD['c']", ["(parseInt((SD['UGS'][num]['level'] + 1)/5) * - 5 + 2 * SD['UGS'][num]['level'] + 2) * (parseInt((SD['UGS'][num]['level'] + 1) / 5) + 1) / 2"]]
+//         ],
+//     },
+//     3 : {
+//         name : "한번에 얻는 보석 개수 증가",
+//         material : ["루비", "SD['ruby']"],
+//         level : 0,
+//         max_level : -1,
+//         price : "20",
+//         price_equation : "SD[\'UGS\'][num][\'price\'] = parseInt((200 + SD['UGS'][num]['level'] * (SD['UGS'][num]['level'] + 1) * 100) * SD['udc'])",
+//         result : [
+//             ["SD['d']", ["(parseInt((SD['UGS'][num]['level'] + 1)/5) * - 5 + 2 * SD['UGS'][num]['level'] + 2) * (parseInt((SD['UGS'][num]['level'] + 1) / 5) + 1) / 2"]]
+//         ],
+//     },
+//     4 : {
+//         name : "오버로드 출현 확률 증가",
+//         material : ["오버로드 주괴", "SD['overlordingot']"],
+//         level : 0,
+//         max_level : 99,
+//         price : "5",
+//         price_equation : "SD[\'UGS\'][num][\'price\'] = 5",
+//         result : [
+//             ["overlordposs", ["SD['UGS'][num]['level'] + 1"]]
+//         ],
+//     },
+//     5 : { // 나중에 분리 예정
+//         name : "한번에 조합하는 일반 아이템 개수 증가",
+//         material : ["다이아몬드","SD['diamond']"],
+//         level : 0,
+//         max_level : 4,
+//         price : 2000,
+//         price_equation : "SD[\'UGS\'][num][\'price\'] = parseInt((2000 + SD['UGS'][num]['level'] * (SD['UGS'][num]['level'] + 1) * 1000) * SD.udc)",
+//         result : [
+//             ["SD.b", "SD['UGS'][num]['level'] + 1"]
+//         ]
+//     },
+//     // num : { // 사용시 '복사'해서 컨트롤 + /(슬래쉬)로 주석해제
+//     //     name : "", //SD.UGS.[num].name
+//     //     material : ["재료이름", "재료경로"],
+//     //     level : 0,
+//     //     max_level : -1, //만렙, 최고레벨 적고, 제한 없을 경우 -1
+//     //     price : "필요한 재료 갯수(숫자)",
+//     //     price_equation : "SD[\'UGS\'][num][\'price\'] = 수식",
+//     //     result : [
+//     //         ["업그레이드로 바뀔것", ["업그레이드로 바뀌게할 수식"]]
+//     //     ],
+//     // },
 // },
-
+// 
 // function UGS_load(num) {
 //     var y = document.getElementsByClassName("UGS_list");
 //     eval(SD["UGS"][num]["price_equation"]); // 가격 레벨에 맞게 업데이트
