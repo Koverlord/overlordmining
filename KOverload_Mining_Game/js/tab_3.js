f8c4e17e6e82a5af0574c92da2f7d0061adbbc2f
Koverlord <76400288+Koverlord@users.noreply.github.com>
2021-04-08 16:26:01 +0900

    WIP

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -1,8 +1,20 @@
 //탭3 : 조합소
 
-function have_no_idea_to_naming(){
+var material = {
+    1 : ['iron','gold']
+};
+
+var price = {
+    1 : ['10', '10']
+    
+var 
+function have_no_idea_to_naming(num){
     let a=prompt("조합할 개수를 입력해 주세요.");
-    alert(a);
     console.log(a);
+    let max_make=parseInt(SD[material[num][0]]/price[num][0]);
+    for(var i=0; i<=material[num].length; i++){
+        if (parseInt(SD[material[num][i]]/price[num][i]) < max_make){
+        max_make = parseInt(SD[material[num][i]]/price[num][i];
+    }
 }
   
