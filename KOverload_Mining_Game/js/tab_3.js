6ca4b1ab1ae771c18ab18edc0385536d5abb399b
Koverlord <76400288+Koverlord@users.noreply.github.com>
2021-04-08 18:10:35 +0900

    Update tab_3.js

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -13,7 +13,7 @@ function have_no_idea_to_naming(num){
     let max_make=parseInt(SD[material[num][0]]/price[num][0]);
     for(var i=0; i<=material[num].length; i++){
         if (parseInt(SD[material[num][i]]/price[num][i]) < max_make){
-        max_make = parseInt(SD[material[num][i]]/price[num][i];
+        max_make = parseInt(SD[material[num][i]]/price[num][i]);
     }
 }
   
