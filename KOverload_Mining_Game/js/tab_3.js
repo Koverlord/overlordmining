3e9abde1c92dc7c03a6a59d0bae94de97f545e23
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-08 17:09:49 +0900

    오류 수정

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -6,15 +6,17 @@ var material = {
 
 var price = {
     1 : ['10', '10']
-    
-var 
+};
+// var 
+
 function have_no_idea_to_naming(num){
-    let a=prompt("조합할 개수를 입력해 주세요.");
+    let a = prompt("조합할 개수를 입력해 주세요.");
     console.log(a);
-    let max_make=parseInt(SD[material[num][0]]/price[num][0]);
-    for(var i=0; i<=material[num].length; i++){
-        if (parseInt(SD[material[num][i]]/price[num][i]) < max_make){
-        max_make = parseInt(SD[material[num][i]]/price[num][i];
+    let max_make = parseInt(SD[material[num][0]] / price[num][0]);
+    for(var i = 0; i<=material[num].length; i++){
+        if (parseInt(SD[material[num][i]] / price[num][i]) < max_make){
+        max_make = parseInt(SD[material[num][i]] / price[num][i]);
+        }
     }
 }
   
