400d7bde752e03ce3bddf31137afd46f2a439d78
Koverlord <alstjtjd05@naver.com>
2021-04-09 13:11:12 +0900

    Update tab_3.js

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -1,32 +1,32 @@
 //탭3 : 조합소
 
-var material = {
-    1 : ['iron','gold']
+const craft_material = {
+    0 : ['iron','gold']
 };
 
-var price = {
-    1 : ['10', '10']
+const craft_price = {
+    0 : ['10', '10']
 };
-var result = {
-    1 : 'irongold'
+const craft_result = {
+    0 : 'irongold'
 };
     
 function craft(num){
 
-    let max_make = parseInt(SD[material[num][0]] / price[num][0]);
-    for(let i = 0; i <= material[num].length; i++){
-        if (parseInt(SD[material[num][i]] / price[num][i]) < max_make){
-        max_make = parseInt(SD[material[num][i]] / price[num][i]);
+    let max_make = parseInt(SD[craft_material[num][0]] / craft_price[num][0]);
+    for(let i = 0; i <= craft_material[num].length; i++){
+        if (parseInt(SD[craft_material[num][i]] / craft_price[num][i]) < max_make){
+        max_make = parseInt(SD[craft_material[num][i]] / craft_price[num][i]);
         }
     }
     let make = prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_make + "개");
     try {
         make = parseInt(make);
         if (0 <= make && make <= max_make){
-            for(let i=0; i <= material[num].length; i++){
-                SD[material[num][i]] -= price[num][i] * make;
+            for(let i=0; i <= craft_material[num].length; i++){
+                SD[craft_material[num][i]] -= craft_price[num][i] * make;
             }
-            SD[result[num]] += make;
+            SD[craft_result[num]] += make;
             alert("조합 완료.");
             store()
         }
