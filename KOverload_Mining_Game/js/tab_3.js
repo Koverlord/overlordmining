132b005091679473b10c94e64e5ce67cd7985b4b
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-09 13:01:39 +0900

    띄어쓰기

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -13,20 +13,20 @@ var result = {
     
 function craft(num){
 
-    let max_make=parseInt(SD[material[num][0]]/price[num][0]);
-    for(let i=0; i<=material[num].length; i++){
-        if (parseInt(SD[material[num][i]]/price[num][i]) < max_make){
-        max_make = parseInt(SD[material[num][i]]/price[num][i]);
+    let max_make = parseInt(SD[material[num][0]] / price[num][0]);
+    for(let i = 0; i <= material[num].length; i++){
+        if (parseInt(SD[material[num][i]] / price[num][i]) < max_make){
+        max_make = parseInt(SD[material[num][i]] / price[num][i]);
         }
     }
-    let make=prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_make + "개");
+    let make = prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_make + "개");
     try {
-        make=parseInt(make);
-        if (0<=make && make<=max_make){
-            for(let i=0; i<=material[num].length; i++){
-                SD[material[num][i]]-=price[num][i]*make;
+        make = parseInt(make);
+        if (0 <= make && make <= max_make){
+            for(let i=0; i <= material[num].length; i++){
+                SD[material[num][i]] -= price[num][i] * make;
             }
-            SD[result[num]]+=make;
+            SD[result[num]] += make;
             alert("조합 완료.");
             store()
         }
