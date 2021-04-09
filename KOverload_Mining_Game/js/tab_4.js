ddb0f5e15782fa6c4ead508cfcb9440d0e1ad8d9
Koverlord <alstjtjd05@naver.com>
2021-04-09 18:08:03 +0900

    Update tab_4.js

---
+++ KOverload_Mining_Game/js/tab_4.js
@@ -0,0 +1,25 @@
+function extract(){
+    oh = SD.overlord;
+    for(let i = 0; i < oh; i++){
+        r_extract = Math.floor(Math.random() * (100)) + 1;
+        if (r_extract >= 51){
+            SD.overlord_essence += 1
+        }
+        else if (r_extract >= 31){
+            SD.overlord_scrap += 1
+        }
+        else if (r_extract >= 2){
+            SD.overlord_part += 1
+        }
+        else if (r_extract == 1){
+            r_extract = Math.floor(Math.random() * (100)) + 1;
+            if (r_extract >= 2){
+                SD.overlord_ingot += 1
+            }
+            else{
+                SD.overlord_soul += 1
+            }
+        }
+    }
+    add_log("추출 완료.")
+}
