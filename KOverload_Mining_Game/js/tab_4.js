cdab8ca15ce50e78f69c922193610e120d685f03
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-09 18:55:10 +0900

    ; 추가 oh 제거 탭5 파일추가

--- KOverload_Mining_Game/js/tab_4.js
+++ KOverload_Mining_Game/js/tab_4.js
@@ -1,27 +1,28 @@
-function extract(){
-    oh = SD.overlord;
-    for(let i = 0; i < oh; i++){
+// 탭4 : 추출소
+
+function extract(){ 
+    for(let i = 0; i < SD.overlord; i++){
         r_extract = Math.floor(Math.random() * (100)) + 1;
         if (r_extract >= 51){
-            SD.overlord_essence += 1
+            SD.overlord_essence += 1;
         }
         else if (r_extract >= 31){
-            SD.overlord_scrap += 1
+            SD.overlord_scrap += 1;
         }
         else if (r_extract >= 2){
-            SD.overlord_part += 1
+            SD.overlord_part += 1;
         }
         else if (r_extract == 1){
             r_extract = Math.floor(Math.random() * (100)) + 1;
             if (r_extract >= 2){
-                SD.overlord_ingot += 1
+                SD.overlord_ingot += 1;
             }
             else {
-                SD.overlord_soul += 1
+                SD.overlord_soul += 1;
             }
         }
     }
     SD.overlord=0;
-    store()
-    add_log("추출 완료.")
+    store();
+    add_log("추출 완료.");
 }
