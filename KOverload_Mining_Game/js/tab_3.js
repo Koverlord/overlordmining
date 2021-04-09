e5c1686e0085c4ae02b54075f7f47287503b1ed1
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-09 13:28:33 +0900

    조합소 수정

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -19,23 +19,25 @@ function craft(num){
         max_make = parseInt(SD[craft_material[num][i]] / craft_price[num][i]);
         }
     }
-    let make = prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_make + "개");
-    try {
-        make = parseInt(make);
+    // let make = prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_make + "개");
+    let make = parseInt("0" + prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_make + "개"));
+
+    // try {
+        // make = parseInt(make);
         if (0 <= make && make <= max_make){
             for(let i=0; i <= craft_material[num].length; i++){
                 SD[craft_material[num][i]] -= craft_price[num][i] * make;
             }
             SD[craft_result[num]] += make;
-            alert("조합 완료.");
+            alert(make + "개 조합 완료.");
             store()
         }
         else {
             alert("제대로 입력하세요 빠빡대가리야");
         }
-    }
-    catch{
-        alert("제대로 입력하세요 빠빡대가리야");
-    }
+    // }
+    // catch{
+    //     alert("제대로 입력하세요 빠빡대가리야");
+    // }
 }
   
