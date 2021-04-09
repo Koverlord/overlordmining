0f65074b8cf3f5c0238d1a6f3a4d649352b35913
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-09 19:07:31 +0900

    주석 추가

--- KOverload_Mining_Game/js/tab_4.js
+++ KOverload_Mining_Game/js/tab_4.js
@@ -2,15 +2,15 @@
 
 function extract() {  //추출
     for (let i = 0; i < SD.overlord; i++) {
-        r_extract = Math.floor(Math.random() * (100)) + 1;
+        let r_extract = Math.floor(Math.random() * (100)) + 1; // 1 ~ 100 난수 생성
         if (r_extract >= 51) {
-            SD.overlord_essence += 1;
+            SD.overlord_essence += 1; //정수
         }
         else if (r_extract >= 31) {
-            SD.overlord_scrap += 1;
+            SD.overlord_scrap += 1; //파편
         }
         else if (r_extract >= 2) {
-            SD.overlord_part += 1;
+            SD.overlord_part += 1; //조각
         }
         else if (r_extract == 1) {
             r_extract = Math.floor(Math.random() * (100)) + 1;
