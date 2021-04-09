70874fb03e762eadef0b01c13c448ae4c53e4367
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-09 13:53:42 +0900

    탭3 수정

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -12,7 +12,6 @@ const craft_result = {
 };
     
 function craft(num){
-
     let max_craft = parseInt(SD[craft_material[num][0]] / craft_price[num][0]);
     for(let i = 0; i <= craft_material[num].length; i++){
         if (parseInt(SD[craft_material[num][i]] / craft_price[num][i]) < max_craft){
@@ -20,24 +19,22 @@ function craft(num){
         }
     }
 
-    let craft = parseInt("0" + prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_craft + "개"));
+    let craft = parseInt("0" + prompt("조합할 개수를 입력해 주세요. 현재 최대 조합 가능 개수 : " + max_craft + "개")); // 음수 써도 0으로 바뀜
 
-    if (craft < 1) {
+    if (craft = 0) {
         add_log("1 이상의 숫자를 입력해 주세요");
-    }
-    else if (craft <= max_craft){
-        for(let i = 0; i <= craft_material[num].length; i++){
-            SD[craft_material[num][i]] -= craft_price[num][i] * craft;
-        }
-        SD[craft_result[num]] += craft;
-        add_log(craft + "개 조합 완료");
-        store();
     }
     else {
         for(let i = 0; i <= craft_material[num].length; i++){
-            SD[craft_material[num][i]] -= craft_price[num][i] * max_craft;
+            if (craft <= max_craft){
+                SD[craft_material[num][i]] -= craft_price[num][i] * craft;
+                SD[craft_result[num]] += craft;
+            }
+            else {
+                SD[craft_material[num][i]] -= craft_price[num][i] * max_craft;
+                SD[craft_result[num]] += max_craft;
+            }
         }
-        SD[craft_result[num]] += craft;
         add_log(craft + "개 조합 완료");
         store();
     }
