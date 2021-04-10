bd840fedffbd9a7f2089809d83470fc2cbc16907
Koverlord <76400288+Koverlord@users.noreply.github.com>
2021-04-11 03:40:04 +0900

    할짓추가 333333333333333

--- KOverload_Mining_Game/js/development_option.js
+++ KOverload_Mining_Game/js/development_option.js
@@ -5,7 +5,8 @@ function test() {
     let test_text = " ";
     test_text += "할짓 모음 <br>"
     test_text += "1. 업그레이드, 조합소 탭 나누기 <br>"
-    test_text += "2. 조합소 재료 쓰기"
+    test_text += "2. 조합소 재료 쓰기 <br>"
+    test_text += "3. 기계들 버프 적용 준비 <br>"
     z.innerHTML = test_text
 }
 
