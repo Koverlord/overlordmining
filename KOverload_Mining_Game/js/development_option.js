bb4a656deef055d6548ee5e6d08ea4a14b67d39b
Koverlord <alstjtjd05@naver.com>
2021-04-11 03:38:44 +0900

    할짓추가 22222222

--- KOverload_Mining_Game/js/development_option.js
+++ KOverload_Mining_Game/js/development_option.js
@@ -5,6 +5,7 @@ function test() {
     let test_text = " ";
     test_text += "할짓 모음 <br>"
     test_text += "1. 업그레이드, 조합소 탭 나누기 <br>"
+    test_text += "2. 조합소 재료 쓰기"
     z.innerHTML = test_text
 }
 
