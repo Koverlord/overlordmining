76b389e76f1a1d6522dd2513ce328ebeb4ba02dc
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-11 20:53:34 +0900

    새로고침시 경고문

--- KOverload_Mining_Game/js/main.js
+++ KOverload_Mining_Game/js/main.js
@@ -138,6 +138,14 @@ function Tab_size() {
     }
 }
 
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = 'It looks like you have been editing something. '
+                            + 'If you leave before saving, your changes will be lost.';
+
+    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+});
+
 window.onload = function () { // 페이지가 다 load 되면 load()함수 실행
     load();
     Tab_size();
