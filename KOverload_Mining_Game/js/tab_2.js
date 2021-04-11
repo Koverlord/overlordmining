4f6c4a0f4cc0f5ceada780010e3cf1b50e02b258
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-04-11 20:26:07 +0900

    제련 오류 수정

--- KOverload_Mining_Game/js/tab_2.js
+++ KOverload_Mining_Game/js/tab_2.js
@@ -1,31 +1,34 @@
 //탭2 - 제련소
 
-function real_melt(ore) {
-    if (SD[ore] >= SD['melt_count']) {
-        SD[ore] -= SD['melt_count'];
-        SD[`${ore}_ingot`] += SD['melt_count'] * SD['melt_multiply'];
+melt_ore = ["iron", "gold"];
+button_melt = document.getElementsByClassName("button_melt");
+
+function real_melt(num) {
+    if (SD[melt_ore[num]] >= SD['melt_count']) {
+        SD[melt_ore[num]] -= SD['melt_count'];
+        SD[`${melt_ore[num]}_ingot`] += SD['melt_count'] * SD['melt_multiply'];
         store(0); // 광물
         store(1); // 주괴
-        // add_log(Name[ore] + " 1개 제련!");
+        // add_log(Name[melt_ore[num]] + " 1개 제련!");
     }
     else {
         add_log("제련 재료가 부족합니다");
-        melt(ore);
+        melt(num);
     }
 }
 
 //melt('iron');
-function melt(ore) {
-    if (Melt[`${ore}`] == 0) {
-        Melt[`${ore}`] = setInterval(real_melt, 1000, ore);
-        this.innerHTML = "중단"
-        add_log(Name[ore] + " 제련 시작");
+function melt(num) {
+    if (Melt[`${melt_ore[num]}`] == 0) {
+        Melt[`${melt_ore[num]}`] = setInterval(real_melt, 1000, num);
+        button_melt[num].innerHTML = "중단"
+        add_log(Name[melt_ore[num]] + " 제련 시작");
     }
     else {
-        clearTimeout(Melt[`${ore}`]);
-        Melt[`${ore}`] = 0;
-        this.innerHTML = "제련"
-        add_log(Name[ore] + " 제련 중지");
+        clearTimeout(Melt[`${melt_ore[num]}`]);
+        Melt[`${melt_ore[num]}`] = 0;
+        button_melt[num].innerHTML = "제련"
+        add_log(Name[melt_ore[num]] + " 제련 중지");
     }
 
 
