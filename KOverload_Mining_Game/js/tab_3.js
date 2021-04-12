cc1e93747e72faa7bfd1d258b37e396c62455674
Koverlord <alstjtjd05@naver.com>
2021-04-12 09:46:20 +0900

    zㅋ

--- KOverload_Mining_Game/js/tab_3.js
+++ KOverload_Mining_Game/js/tab_3.js
@@ -86,12 +86,12 @@ function craft(num) {
                 SD[craft_material[num][i]] -= craft_price[num][i] * craft;
             }
             if (num == 4 || num == 5 || num == 6) { // 오버로드 아이템은 조합버프를 받지 않아야 하므로 오버로드 아이템의 번호를 적어둘것
-                SD[craft_result[num]] += craft * SD['b'];
-                add_log(craft * SD['b'] + "개 조합 완료");
-            }
-            else { // 그외
                 SD[craft_result[num]] += craft;
                 add_log(craft + "개 조합 완료");
+            }
+            else { // 그외
+                SD[craft_result[num]] += craft * SD['b'];
+                add_log(craft * SD['b'] + "개 조합 완료");
         }
     }
         store(1); // 주괴
