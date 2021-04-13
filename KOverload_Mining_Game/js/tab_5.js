3c6804b3913a93f08b341f5bd8b0913657114586
Koverlord <alstjtjd05@naver.com>
2021-04-14 08:07:22 +0900

    오

--- KOverload_Mining_Game/js/tab_5.js
+++ KOverload_Mining_Game/js/tab_5.js
@@ -8,3 +8,23 @@ function tab_robot(num) {
     robot_list[num].style.display = "block";
     tab_robot_num = num;
 }
+
+const robot_material = {
+    0 : ["alloy_iron_gold", "overlord_ingot"],
+    1 : ["iron_ingot", "gold_ingot"],
+    2 : ["enchantedoverlordingot", "circuit"],
+    3 : ["diamond", "overlord_ingot"]
+};
+
+const robot_price = {
+    0 : ["1000", "100"],
+    1 : ["30", "30"],
+    2 : ["1", "300"],
+    3 : ['2000', "100"]
+};
+const robot_result = {
+    0 : "robot_body",
+    1 : "circuit",
+    2 : "CPU",
+    3 : "mining_drill"
+};
