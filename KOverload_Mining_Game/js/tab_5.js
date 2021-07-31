e6cb1ccca57140afb5dea5dfd4cdafc20dc712ed
rich-chanel <74828344+rich-chanel@users.noreply.github.com>
2021-08-01 00:23:30 +0900

    let -> const
    언제 수정한 건지는 모름

--- KOverload_Mining_Game/js/tab_5.js
+++ KOverload_Mining_Game/js/tab_5.js
@@ -2,8 +2,8 @@
 
 tab_robot_num = 0;
 function tab_robot(num) {
-    let tab_robot_list = document.getElementsByClassName("tab_robot")
-    let robot_list = document.getElementsByClassName("robot");
+    const tab_robot_list = document.getElementsByClassName("tab_robot")
+    const robot_list = document.getElementsByClassName("robot");
     tab_robot_list[tab_robot_num].className = "tab_robot not_active";
     robot_list[tab_robot_num].style.display = "none";
     tab_robot_list[num].className = "tab_robot active";
