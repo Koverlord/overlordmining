f1a628a31f3c9940b2f56d0165cbb5cf6616fd23
Koverlord <alstjtjd05@naver.com>
2021-08-26 16:37:09 +0900

    오버로드주괴는 왜 안올라감

--- KOverload_Mining_Game/js/tab_1.js
+++ KOverload_Mining_Game/js/tab_1.js
@@ -85,7 +85,7 @@ function UGS_load(num) { // 업그레이드 상점 로드 시에 가격 텍스�
             UGS_price[num][0] = parseInt((SD.UGS_lvl[num] + 1) * 500);
             UGS_price[num][1] = parseInt(30 + SD.UGS_lvl[num] * (SD.UGS_lvl[num] + 1) * 15);
             UGS_price[num][2] = parseInt(10 + SD.UGS_lvl[num] * (SD.UGS_lvl[num] + 1) * 10);
-            UGS_price[num][3] = parseInt(50 + SD.UGS_lvl[num] * (SD.UGS_lvl[num] + 1) * 25);
+            UGS_price[num][3] = parseInt(50 + SD.UGS_lvl[num] * (SD.UGS_lvl[num] + 1) * 450);
             SD.extractor_level = SD.UGS_lvl[num] + 1;
             UGS_text += "오버로드 1개당 추출 아이템 수 증가. level : " + SD.UGS_lvl[num] + "<br>";
         // case num: // 설명
